b320647942157822bc2d016ee5ab146337917bd8
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2024-06-04 07:46:41 +0800

    Simplified code

--- senderror.js
+++ senderror.js
@@ -1,6 +1,6 @@
 const { nanoid } = require("nanoid");
 const connection = require("./connection");
-const { servererror, success } = require("./response");
+const { servererror } = require("./response");
 
 async function senderror (req, res, errorparams, name) {
   const { stack } = errorparams;
@@ -12,9 +12,7 @@ async function senderror (req, res, errorparams, name) {
     createdaterror: createdat,
   };
   const query = 'INSERT INTO errorkad SET ?';
-  connection.query(query, data, (error) => {
-    res.response(success('Error berhasil ditambahkan')).code(201);
-  });
+  await connection.query(query, data);
   return res.response(servererror(stack));
 }
 
